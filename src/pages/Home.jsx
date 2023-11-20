22cfaece5371669be7ec4f978127aa2f714a2e5d
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-11-20 16:28:27 +1100

    adds initial component and pages structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Vege from "../components/Vege";
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function Home() {
+return (
+    <div>
+        <Vege />
+        <Popular />
+    </div>
+)
+}
+
+export default Home;
