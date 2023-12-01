9a3e7e7939f6d9e3ec6c34acdbb19a17f577e402
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-12-01 14:08:02 +1100

    creates style for dish cards, applied to popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -23,9 +23,9 @@ function Popular() {
       <Wrapper>
         <h3>Popular Dishes</h3>
         <Splide options={{
-          perPage: 4, 
-          arrows: false, 
-          pagination: false,
+          perPage: 4,
+          arrows: false,
+          pagination: false, 
           drag:"free",
           gap:"5rem"
         }}>
@@ -34,6 +34,7 @@ function Popular() {
           <Card key={recipe.id}>
             <p>{recipe.title}</p>
             <img src={recipe.image} alt={recipe.title} />
+            <Gradient />
           </Card>
           </SplideSlide>
         ))}
@@ -51,10 +52,40 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute; 
+    left: 0;
+    width: 100%; 
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute; 
+    z-index: 10; 
+    left: 50%; 
+    bottom: 0%; 
+    transform: translate(-50%, 0%);
+    color: white; 
+    width: 100%; 
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem; 
+    height: 40%; 
+    display: flex;
+    justify-content: center;
+    align-items: center; 
   }
 `;
+
+const Gradient = styled.div`
+z-index: 3;
+position: absolute;
+width: 100%; 
+height: 100%; 
+background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9));`
 
 export default Popular;
